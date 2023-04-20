29ac04ec10bb6ea3ecb298fbeb3b83a07d99ce6d
Osayuki's Work <oethe68@gmail.com>
2023-04-20 13:43:51 -0400

    Part 6/7 still in progress. Video 271

--- app/src/main/java/com/example/readersapp/screens/update/UpdateScreen.kt
+++ app/src/main/java/com/example/readersapp/screens/update/UpdateScreen.kt
@@ -1,23 +1,25 @@
 package com.example.readersapp.screens.update
 
 import android.util.Log
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.LinearProgressIndicator
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import coil.compose.rememberImagePainter
 import com.example.readersapp.components.ReadersAppBar
 import com.example.readersapp.data.DataOrException
 import com.example.readersapp.model.MBook
@@ -29,38 +31,138 @@ private const val TAG = "UpdateScreen"
 fun UpdateScreen(navController: NavHostController, bookItemId: String,
                  viewModel: HomeScreenViewModel = hiltViewModel()) {
     Scaffold(topBar = {
-        ReadersAppBar(title = "Update Book",
+        ReadersAppBar(
+            title = "Update Book",
             icon = Icons.Default.ArrowBack,
             showProfile = false,
-            navController = navController){
+            navController = navController
+        ) {
             navController.popBackStack()
         }
     }) {
 
         val bookInfo = produceState<DataOrException<List<MBook>, Boolean,
-                Exception>>(initialValue = DataOrException(data = emptyList(),
-            loading = true, e = Exception(""))){
-                    value =  viewModel.data.value
+                Exception>>(
+            initialValue = DataOrException(
+                data = emptyList(),
+                loading = true, e = Exception("")
+            )
+        ) {
+            value = viewModel.data.value
         }.value
-        
-        Surface(modifier = Modifier
-            .padding(top = 3.dp)
-            .fillMaxSize()) {
-            
-            Column(modifier = Modifier
-                .padding(top = 3.dp),
-            verticalArrangement = Arrangement.Top,
-            horizontalAlignment = Alignment.CenterHorizontally) {
+
+        Surface(
+            modifier = Modifier
+                .padding(top = 3.dp)
+                .fillMaxSize()
+        ) {
+
+            Column(
+                modifier = Modifier
+                    .padding(top = 3.dp),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
                 Log.d(TAG, "UpdateScreen: ${viewModel.data.value.data.toString()}")
-                if (bookInfo.loading == true){
+                if (bookInfo.loading == true) {
                     LinearProgressIndicator()
                     bookInfo.loading = false
-                }else{
-                    Text(text = viewModel.data.value.data?.get(0)?.title.toString())
+                } else {
+                    Surface(
+                        modifier = Modifier
+                            .padding(2.dp)
+                            .fillMaxWidth(),
+                        shape = CircleShape,
+                        elevation = 4.dp
+                    ) {
+                        ShowBookUpdate(bookInfo = viewModel.data.value, bookItemId = bookItemId)
+                    }
+                    ShowSimpleForm(book = viewModel.data.value.data?.first { mBook ->
+                        mBook.googleBookId == bookItemId
+                    })
                 }
+
             }
-            
+
         }
-        
     }
 }
+
+
+@Composable
+fun ShowBookUpdate(
+    bookInfo: DataOrException<List<MBook>, Boolean, Exception>,
+    bookItemId: String
+) {
+    Row() {
+        Spacer(modifier = Modifier.width(43.dp))
+        if (bookInfo.data != null){
+            Column(modifier = Modifier.padding(4.dp), verticalArrangement = Arrangement.Center) {
+                CardListItem(book = bookInfo.data!!.first{ mBook ->
+                    mBook.googleBookId == bookItemId
+
+                }, onPressDetails = {})
+            }
+        }
+    }
+}
+
+@Composable
+fun CardListItem(book: MBook, onPressDetails: () -> Unit) {
+    Card(modifier = Modifier
+        .padding(
+            start = 4.dp,
+            end = 4.dp,
+            top = 4.dp,
+            bottom = 8.dp
+        )
+        .clip(RoundedCornerShape(20.dp))
+        .clickable { },
+        elevation = 8.dp
+    ) {
+        Row(horizontalArrangement = Arrangement.Start) {
+            Image(painter = rememberImagePainter(data = book.photoUrl.toString()),
+                contentDescription = null,
+                modifier = Modifier
+                    .height(100.dp)
+                    .width(120.dp)
+                    .padding(4.dp)
+                    .clip(
+                        RoundedCornerShape(
+                            topStart = 120.dp,
+                            topEnd = 20.dp,
+                        )
+                    ))
+            Column {
+                Text(text = book.title.toString(),
+                    style = MaterialTheme.typography.h6,
+                    modifier = Modifier
+                        .padding(start = 8.dp, end = 8.dp)
+                        .width(120.dp),
+                    fontWeight = FontWeight.SemiBold,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis)
+
+                Text(text = book.authors.toString(),
+                    style = MaterialTheme.typography.body2,
+                    modifier = Modifier.padding(start = 8.dp,
+                    end = 8.dp,
+                    top = 2.dp,
+                    bottom = 0.dp))
+
+                Text(text = book.publishedDate.toString(),
+                    style = MaterialTheme.typography.body2,
+                    modifier = Modifier.padding(start = 8.dp,
+                        end = 8.dp,
+                        top = 0.dp,
+                        bottom = 8.dp))
+
+            }
+        }
+
+    }
+}
+
+
+
+
